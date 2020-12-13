e7b070624080bd945740682fa1cb784b53f40037
Ndunge M <ndunge_mulwa@brown.edu>
2020-12-13 15:40:38 -0500

    adding eye and nose buttons

--- face_detection.py
+++ face_detection.py
@@ -1,14 +1,22 @@
 import numpy as np
 import cv2 as cv
-from Tkinter import *
-from PIL import Image
+from tkinter import  Tk, Label, Button, StringVar
+from PIL import Image, ImageTk
 
+def EyeOverlap():
+    print("Hello World!")
+    # cv.setMouseCallback('image',overlap_eye_function)
+def NoseOverlap():
+    print("Hello Nose!")
+    # cv.setMouseCallback('image',overlapNoseFunctions)
 faceCascade = cv.CascadeClassifier('xml/haarcascade_frontalface_default.xml')
+
+global panelA
+panelA = None
+
 cap = cv.VideoCapture(0)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
-global panelA
-
 while True:
     ret, img = cap.read()
     img = cv.flip(img, -1)
@@ -24,7 +32,8 @@ while True:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]  
     # cv.imshow('video',img)
-    
+    root = Tk()
+
     # OpenCV represents images in BGR order; however PIL represents
     # images in RGB order, so we need to swap the channels
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
@@ -46,19 +55,29 @@ while True:
         panelA.image = img
 
     # initialize the window toolkit along with the two image panels
-    root = Tk()
     panelA = None
     panelB = None
     # create a button, then when pressed, will trigger a file chooser
     # dialog and allow the user to select an input image; then add the
     # button the GUI
-    btn = Button(root, text="Select an image")
-    btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+    eye = Image.open("eyeball.png")
+    phEye = ImageTk.PhotoImage(eye)
+    nose = Image.open("nose.png")
+    phNose = ImageTk.PhotoImage(nose)
+    var = StringVar()
+    label = Label( root, textvariable=var,padx="10", pady="10" )
+    var.set("Select an image")
+    label.pack()
+    btnEye = Button(root, text="Eyes", image=phEye, command=EyeOverlap)
+    btnNose = Button(root, text="Eyes", image=phNose, command=NoseOverlap)
+    btnEye.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+    btnNose.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
     # kick off the GUI
     root.mainloop()
     
-    # k = cv.waitKey(30) & 0xff
-    # if k == 27: # press 'ESC' to quit
-    #     break
+    k = cv.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
 cap.release()
 cv.destroyAllWindows()
